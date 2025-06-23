949c9e895e8530512cc7be5085a53bc7440d6ae3
johnkirchenbauer69 <112353495+johnkirchenbauer69@users.noreply.github.com>
2025-06-23 15:29:07 -0500

    server.js

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
 
 // In-memory cache
 let listingsCache = [];
